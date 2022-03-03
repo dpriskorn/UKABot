43a2edc5638eb6f8337a63d9b169c02606f3140a
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-03-03 09:42:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# UKABot
+
+Bot for Wikidata that imports 
+[UKÄ classifiers](https://www.wikidata.org/wiki/Property:P10361) 
+for scientific articles already in Wikidata
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+pseudo code
+for each row in swepub.pkl
+    lookup doi in WD
+    if found
+        upload P10361 to WD using WBI
+        update swepub.pkl and mark done uka_done=True
+"""
